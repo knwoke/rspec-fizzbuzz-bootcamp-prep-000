e24bc7af22275194dc9922590371f6043f121fbf
Kevin Nwoke <kevinnwoke@yahoo.com>
2018-08-26 21:37:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,18 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
   if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+    return "Fizz" # Go fizz
   elsif int % 5 == 0
-    "Buzz"
+    return "Buzz"
   elsif int % 3&&5 == 0
-    "Fizzbuzz"
+    return "Fizzbuzz"
   else
-    nil
-  end
-end
-
-def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+    return nil
   end
 end
