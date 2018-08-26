9d2bba6695ec854494f8e5083466e6121df14e3a
Kevin Nwoke <kevinnwoke@yahoo.com>
2018-08-26 21:37:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz (int)
     return "Fizz" # Go fizz
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3&&5 == 0
+  elsif int % 3 && int % 5 == 0
     return "Fizzbuzz"
   else
     return nil
